1d3cff95f38c69f4b7568eb6e34d07ff49ef37f3
Froelicher Jean-Philippe <froelicher.jeanphilippe@gmail.com>
2015-03-13 00:06:52 +0100

    up2

--- test/test.auth.js
+++ test/test.auth.js
@@ -240,7 +240,7 @@ describe('Authentication', function() {
 
     before(function() {
       var hash = "access_token=ew35h4pk0xg7iy1" +
-             "&scope=user_read+channel_read&state=user_dayjay";
+             "&scope=user_read+channel_read+user_follows_edit&state=user_dayjay";
 
       document.location.hash = hash;
     });
